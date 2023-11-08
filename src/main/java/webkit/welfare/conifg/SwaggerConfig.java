2746a8460ea0122c78d931176076c6aa872cb013
jonghyeon <kiri6077@naver.com>
2023-11-08 15:28:13 +0900

    swagger 작성

--- src/main/java/webkit/welfare/conifg/SwaggerConfig.java
+++ src/main/java/webkit/welfare/conifg/SwaggerConfig.java
@@ -2,13 +2,20 @@ package webkit.welfare.conifg;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -21,6 +28,33 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("webkit.welfare.controller")) // Controller가 들어있는 패키지. 이 경로의 하위에 있는 api만 표시됨.
                 .paths(PathSelectors.any()) // 위 패키지 안의 api 중 지정된 path만 보여줌. (any()로 설정 시 모든 api가 보여짐)
+                .build()
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext(), securityContext2(), securityContext3()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer Token", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(new SecurityReference("Bearer Token", new AuthorizationScope[0])))
+                .forPaths(PathSelectors.ant("/auth/getUser"))
+                .build();
+    }
+
+    private SecurityContext securityContext2() {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(new SecurityReference("Bearer Token", new AuthorizationScope[0])))
+                .forPaths(PathSelectors.ant("/auth/updateUser"))
+                .build();
+    }
+
+    private SecurityContext securityContext3() {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(new SecurityReference("Bearer Token", new AuthorizationScope[0])))
+                .forPaths(PathSelectors.ant("/auth/updatePassword"))
                 .build();
     }
 
--- src/main/java/webkit/welfare/dto/AddUserRequest.java
+++ src/main/java/webkit/welfare/dto/AddUserRequest.java
@@ -1,5 +1,7 @@
 package webkit.welfare.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.Email;
@@ -9,28 +11,36 @@ import java.util.Date;
 
 
 // 회원 가입 시 입력한 항목들의 제약사항을 확인하기 위해 사용
+@ApiModel(description = "회원가입용 유저 정보 도메인")
 @Data
 @Builder
 @AllArgsConstructor
 public class AddUserRequest {
+    @ApiModelProperty(notes = "이메일")
     @Email
     private final String email;
 
+    @ApiModelProperty(notes = "이름")
     @NotBlank
     private final String username;
 
+    @ApiModelProperty(notes = "비밀번호")
     @NotBlank
     private final String password;
 
+    @ApiModelProperty(notes = "시도명")
     @NotBlank
     private final String ctpvNm;
 
+    @ApiModelProperty(notes = "시군구명")
     @NotBlank
     private final String sggNm;
 
+    @ApiModelProperty(notes = "생년월일")
     @NotNull
     private final Date birth;
 
+    @ApiModelProperty(notes = "가구상황")
     @NotBlank
     private final String familySituation;
 
